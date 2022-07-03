91da7136eba66785cfcf0b25f4b09814f592f422
Mohammad Pourmami <m.pourmami@gmail.com>
2022-07-03 17:59:59 +0430

    use a Json file as database to save and read products from it

--- models/product.js
+++ models/product.js
@@ -1,4 +1,15 @@
-const products = [];
+const fs = require('fs')
+const path = require('path')
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err)
+            cb([])
+        else
+            cb(JSON.parse(fileContent))
+    })
+}
 
 module.exports = class Product {
     constructor(t) {
@@ -6,10 +17,15 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        getProductsFromFile(products => {
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+            })
+        })
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        getProductsFromFile(cb)
     }
 }
