dc4d3ba18447be7322bb399ad337fce1a761487d
Mohammad Pourmami <M.Pourmami@gmail.com>
2022-07-31 20:13:46 +0430

    connect project to sql database

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,20 +2,22 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll().then(([products, fieldData]) => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
       path: '/products'
     });
+  }).catch((err) => {
+    console.log(err)
   });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findById(prodId).then(([product]) => {
     res.render('shop/product-detail', {
-      product: product,
+      product: product[0],
       pageTitle: product.title,
       path: '/products'
     });
@@ -23,12 +25,14 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll().then(([rows, fieldData]) => {
     res.render('shop/index', {
-      prods: products,
+      prods: rows,
       pageTitle: 'Shop',
       path: '/'
     });
+  }).catch(err => {
+    console.log(err)
   });
 };
 
